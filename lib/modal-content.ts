c2a71c7ed9d1dc93104d896af6b25da5ee5abe95
Timilehin Durotoye <tcd5276@psu.edu>
2025-12-09 18:55:04 -0500

    Update modal-content.ts

--- lib/modal-content.ts
+++ lib/modal-content.ts
@@ -7,7 +7,7 @@ interface ModalContent {
 export const welcomeModalContent: ModalContent = {
   brief: 'Welcome to Daily News - AI-Powered Journalism',
   image: '/ai-daily.png',
-  text: `Daily News is an AI-driven online news website built to keep you
+  text: `The Daily News is an AI-driven online news website built to keep you
 informed about local, national, and global events and affairs around
 you. We employ an artificial intelligence (AI) system that is
 custom-designed and trained for news curation and production. As with
